ff5c56e76c5ffedc68981b9082f13abd47636ae9
felipemichelin <contato@felipemichelin.com>
2022-03-18 18:54:55 -0300

    v

--- functions/redirect.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async function (event) {
-  const fb_pixel = process.env.FB_PIXEL;
-  return {
-      statusCode: 302,
-      headers: {
-          "Location": "/?fb=" + fb_pixel + new URLSearchParams(event.queryStringParameters),
-      },
-  };
-}
