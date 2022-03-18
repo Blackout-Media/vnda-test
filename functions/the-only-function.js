cbc3db69becf9e728748270a340e4d0977217018
felipemichelin <contato@felipemichelin.com>
2022-03-18 20:32:25 -0300

    a

--- functions/the-only-function.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async function(event, context) {
-  if (event.headers["X-Variant"] === "first-thing") {
-      console.log("a")
-  } else if (event.headers["X-Variant"] === "second-thing") {
-      // do the other thing
-  } else {
-      // return 400 status, because the function was invoked raw
-  }
-}
