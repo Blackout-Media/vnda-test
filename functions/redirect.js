c1a455a24c1126c34288932d67e43bea54831c22
felipemichelin <contato@felipemichelin.com>
2022-03-18 19:59:42 -0300

    Update redirect.js

--- functions/redirect.js
+++ functions/redirect.js
@@ -1,3 +1,7 @@
-exports.handler = async function (event) {
-console.log("papa");
+// modern JS style - encouraged
+export async function handler(event, context) {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: `Hello world ${Math.floor(Math.random() * 10)}` })
+  };
 }
