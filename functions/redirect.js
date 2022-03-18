65d3cafbc4c4487c2cfe946efa2738d50597a32a
felipemichelin <contato@felipemichelin.com>
2022-03-18 19:52:09 -0300

    Update redirect.js

--- functions/redirect.js
+++ functions/redirect.js
@@ -1,9 +1,3 @@
 exports.handler = async function (event) {
-  const lang = event.queryStringParameters.state?.split("-")[0] ?? "en";
-  return {
-      statusCode: 302,
-      headers: {
-          "Location": "/" + lang + event.path + "?" + new URLSearchParams(event.queryStringParameters),
-      },
-  };
+    console.log("hehe");
 }
